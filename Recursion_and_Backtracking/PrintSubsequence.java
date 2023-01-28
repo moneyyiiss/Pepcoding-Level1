cbb785ff0a2522a78d248790f7608b1883c01f52
Manish Mani Dinkar <moneyyiiss@gmail.com>
2023-01-28 08:03:20 +0530

    updated recursion and backtracking

---
+++ Recursion_and_Backtracking/PrintSubsequence.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class PrintSubsequence {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        printSS(str, "");
+    }
+
+    public static void printSS(String ques, String ans){
+        if(ques.length() == 0){
+            System.out.println(ans);
+            return;
+        }
+
+        char ch = ques.charAt(0);
+        String roq = ques.substring(1);
+        printSS(roq, ans + ch);
+        printSS(roq, ans + "");
+    }
+}
